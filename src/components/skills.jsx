6264ba6b0678b4c01c28ea17f474d7cd48042928
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-06-22 18:21:28 +0300

    motion-commit

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,41 +1,71 @@
+import { motion } from 'motion/react';
+
+
+const parent={
+ hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.15
+    }
+ 
+}
+}
+const child={
+   hidden: {
+    opacity: 0,
+    y: 40,
+    scale: 0.96
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut"
+    }
+  }
+}
+
 export default function Skills(){
     return(
         <section className="container" id="skills" >
 
   <h2 id="service_header">Core Skills</h2>
 
-  <div className="columns_skill trigger">
+  <motion.div className="columns_skill trigger" variants={parent} initial='hidden' whileInView="visible"
+    viewport={{ once: true }}>
 
-    <div className="card skills">
+    <motion.div className="card skills" variants={child}>
       <h3>Responsive Interfaces</h3>
       <p>
        Fast, adaptive layouts optimized for mobile, tablet, and desktop experiences.
       </p>
-    </div>
+    </motion.div>
 
-    <div className="card skills">
+    <motion.div className="card skills" variants={child}>
       <h3>React Frontend Development</h3>
       <p>
        Scalable React applications with clean architecture and reusable components.
 
       </p>
-    </div>
+    </motion.div>
 
-    <div className="card skills">
+    <motion.div className="card skills" variants={child}>
       <h3>API Integration</h3>
       <p>
       Secure and efficient integration with REST APIs and third-party services.
       </p>
-    </div>
+    </motion.div>
 
-    <div className="card skills">
+    <motion.div className="card skills" variants={child}>
       <h3>UI/UX Focus</h3>
       <p>
        Minimal, intuitive interfaces designed for clarity, usability, and engagement.
       </p>
-    </div>
+    </motion.div>
 
-  </div>
+  </motion.div>
 
 </section>
     )

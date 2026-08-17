a920879789d7576903e8a150947d2f422ff84e2e
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-08-17 11:41:09 +0300

    new-color-palette

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
-import commerce from '../assets/img/commerce.avif';
+import commerce from '../assets/img/2.jpeg';
 import school from '../assets/img/school.avif';
 import blog from '../assets/img/blog.avif';
+import booking from '../assets/img/booking.png'
 import { motion } from 'motion/react';
 
 
@@ -49,10 +50,10 @@ export default function Projects() {
         
       </h3>
       <img src={commerce} alt='man-in-sweater' className='img'
-      sizes='(max-width: 768px) 100vw, 361px'/>
-      <p>A responsive e-commerce platform that enables users to browse products,
+     />
+      <p className='pro-para'>A responsive e-commerce platform that enables users to browse products,
          manage their cart, and enjoy a smooth online shopping experience."</p>
-      <a className='button .project-demo-btn' href="https://e-commerce-website-sepia-five.vercel.app/"  target="_blank"
+      <a className='button .project-demo-btn' href="https://fresco-iota.vercel.app/"  target="_blank"
   rel="noopener noreferrer">
     View Demo
     </a>
@@ -66,7 +67,7 @@ export default function Projects() {
       </h3>
        <img src={blog} alt='beach' className='img'
       sizes='(max-width: 768px) 100vw, 361px'/>
-      <p>Travel Blog Website — Built with Django and Python,
+      <p className='pro-para'>Travel Blog Website — Built with Django and Python,
          allowing users to explore destinations and read travel articles through a responsive web experience.</p>
        <a className='button' href="https://my-django-project-ynus.onrender.com/"  target="_blank" rel="noopener noreferrer">
           View Demo
@@ -82,12 +83,27 @@ export default function Projects() {
       </h3>
        <img src={school} alt='teacher' className='img'
       sizes='(max-width: 768px) 100vw, 361px'/>
-      <p>Developed a full-stack student grading management system using React and Django,
+      <p className='pro-para'>Developed a full-stack student grading management system using React and Django,
          enabling teachers to create, view, update, and delete student records and grades through a secure and efficient interface.</p>
     <a className='button' href="https://crud-alpha-virid.vercel.app/"  target="_blank" rel="noopener noreferrer">
     View Demo
     </a>
     </motion.div>
+
+    <motion.div className="card project-card" variants={child}>
+      <h3>
+      Booking System
+      </h3>
+       <img src={booking} alt='beach' className='img'
+      sizes='(max-width: 768px) 100vw, 361px'/>
+      <p className='pro-para'>A modern React-based appointment booking website that allows users to browse available services,
+       select convenient dates and times, and book appointments online.</p>
+       <a className='button' href="https://dental-three-gray.vercel.app/"  target="_blank" rel="noopener noreferrer">
+          View Demo
+        </a>
+    </motion.div>
+  
+    
 
   </motion.div>
 </section>
